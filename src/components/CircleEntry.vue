d69c81bf7e304e3170938380701409da73ac96a5
jofwu <jofwu@yahoo.com>
2019-11-06 15:57:37 -0500

    Update CircleEntry.vue

--- src/components/CircleEntry.vue
+++ src/components/CircleEntry.vue
@@ -34,7 +34,7 @@ export default {
       return {
         fill: merged.color,
         fontStyle: merged.style,
-        opacity: this.entry.active ? 1 : 0.4,
+        opacity: this.entry.active ? 1 : 0.2,
       };
     },
   },
