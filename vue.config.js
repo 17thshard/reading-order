726e86ef476e5050914d84c805ae9a7e538cbbff
Marvin Rösch <marvinroesch99@gmail.com>
2019-11-06 10:23:18 +0100

    Enable PWA building

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   publicPath: '/reading-order/',
+  pwa: {
+    name: 'Cosmere Reading Order',
+    themeColor: '#3A69CF',
+    msTileColor: '#3A69CF',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'white',
+    manifestOptions: {
+      background_color: '#FFFFFF',
+    },
+  },
 };
