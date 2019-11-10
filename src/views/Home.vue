a27bb199e58380103f46af324952d4bb0756089f
Marvin Rösch <marvinroesch99@gmail.com>
2019-11-10 20:29:40 +0100

    Allow loading data files locally via drag & drop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
-<div class="home">
+<div
+  class="home"
+  @drop.capture.stop.prevent="loadLocalFile"
+  @dragenter.capture="captureDrop"
+  @dragover.capture="captureDrop"
+>
   <Legend
     :connection-types="Object.values(connectionTypes)"
     :categories="Object.values(categories)"
@@ -40,22 +45,54 @@ export default {
   },
   async mounted() {
     const result = await (await fetch('./data.json')).json();
-    const {
-      books, sorted, connectionTypes, categories,
-    } = loader(result);
-    this.entries = books;
-    this.books = books;
-    this.sortedBooks = sorted;
-    this.connectionTypes = connectionTypes;
-    this.categories = categories;
+    this.loadData(result);
   },
   methods: {
+    loadData(data) {
+      const {
+        books, sorted, connectionTypes, categories,
+      } = loader(data);
+      this.entries = books;
+      this.books = books;
+      this.sortedBooks = sorted;
+      this.connectionTypes = connectionTypes;
+      this.categories = categories;
+    },
     sort(books) {
       this.entries = books === false ? this.books : books;
     },
     toggleExplanations(value) {
       this.explainConnections = value;
       window.localStorage.setItem('explainConnections', value);
+    },
+    loadLocalFile(event) {
+      const file = event.dataTransfer.files[0];
+
+      if (file === undefined || !file.type.match('application.json')) {
+        return;
+      }
+
+      event.preventDefault();
+      event.stopPropagation();
+
+      const reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (e) => {
+        const { result } = e.target;
+        try {
+          this.loadData(JSON.parse(result));
+        } catch (error) {
+          alert(`Could not load data JSON: ${error}`);
+        }
+      };
+
+      // Read in the image file as a data URL.
+      reader.readAsText(file);
+    },
+    captureDrop(event) {
+      event.dataTransfer.dropEffect = 'move';
+      event.preventDefault();
     },
   },
 };
