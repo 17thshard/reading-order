5971eff87d4de53ed9695fdca0566087c248b01b
jofwu <jofwu@users.noreply.github.com>
2024-04-09 13:07:40 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.3c9c2ff0d0b95c7102040d68b68c7ab0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf3f4330d7fa722e752723c6ee6b970",
+    "url": "/reading-order/browserconfig.xml"
+  },
+  {
+    "revision": "442013fc636fbe688bd9",
+    "url": "/reading-order/css/app.ca896a2a.css"
+  },
+  {
+    "revision": "84ca9c24bb8242f05255a65fefb66e82",
+    "url": "/reading-order/data.json"
+  },
+  {
+    "revision": "a50051a3215eb5194943e34babf32f5e",
+    "url": "/reading-order/img/cosmere.a50051a3.svg"
+  },
+  {
+    "revision": "0e6172a1a52cdd11b60f741d83e705a4",
+    "url": "/reading-order/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "50e8bc9c0d2ddbd630d6d8d442030467",
+    "url": "/reading-order/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "3d5cede3e3113a1a5fc3170f022cc947",
+    "url": "/reading-order/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "c9af9e66eb66ac3b2e8607d4370c753d",
+    "url": "/reading-order/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "a06bf34e2c5cfae185f4de05cfa5cdab",
+    "url": "/reading-order/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "5c233105e8261a10c8eb7e6aec0adda6",
+    "url": "/reading-order/img/icons/favicon.ico"
+  },
+  {
+    "revision": "6dfc58ce20b451bd95b5f8b4903af320",
+    "url": "/reading-order/img/icons/mstile-144x144.png"
+  },
+  {
+    "revision": "3f15cc2f64307be0b025eddefec0216a",
+    "url": "/reading-order/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "e7a4a456480f481557878dc1fb6125d0",
+    "url": "/reading-order/img/icons/mstile-310x150.png"
+  },
+  {
+    "revision": "e03bc4bd8050b49ef218e51e185d9b1e",
+    "url": "/reading-order/img/icons/mstile-310x310.png"
+  },
+  {
+    "revision": "16885d32bc48b844e3cfb922cf40bd2a",
+    "url": "/reading-order/img/icons/mstile-70x70.png"
+  },
+  {
+    "revision": "f2e463b8d1c45480c23eb5f7bf6e98a5",
+    "url": "/reading-order/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "442013fc636fbe688bd9",
+    "url": "/reading-order/js/app.139decfd.js"
+  },
+  {
+    "revision": "32d2e058caa3c20ebd0c",
+    "url": "/reading-order/js/chunk-vendors.d80cf524.js"
+  },
+  {
+    "revision": "abda5f466f677bb9dd0037c5b87acd28",
+    "url": "/reading-order/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/reading-order/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reading-order/precache-manifest.3c9c2ff0d0b95c7102040d68b68c7ab0.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "reading-order"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^$|^\/$|\/#.*$/, new workbox.strategies.NetworkFirst(), 'GET');
